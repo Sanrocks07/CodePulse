565a1d93046970eaac11065f93aa3a555211e4cc
Sanrocks07 <sanchitpillai@gmail.com>
2025-04-20 22:22:17 +0530

    Add Category functionality

---
+++ UI/codepulse/src/app/features/category/add-category/add-category.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnDestroy } from '@angular/core';
+import { AddCategoryRequest } from '../models/add-category-request.model';
+import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-add-category',
+  templateUrl: './add-category.component.html',
+  styleUrls: ['./add-category.component.css']
+})
+export class AddCategoryComponent implements OnDestroy {
+
+  model: AddCategoryRequest;
+  private addCategorySubscription?: Subscription;
+
+  constructor(private categoryService: CategoryService) {
+    this.model = {
+      name: '',
+      urlHandle: ''
+    };
+  }
+
+  onFormSubmit() {
+    this.addCategorySubscription = this.categoryService.addCategory(this.model).subscribe({
+      next:(response) => {
+        console.log('Category added successfully', response);
+      },
+      error:(error) => {
+        console.log(error);
+      }
+    });
+
+  }
+
+  ngOnDestroy(): void {
+    // Unsubscribe from the subscription to prevent memory leaks
+    this.addCategorySubscription?.unsubscribe();
+  }
+
+}
